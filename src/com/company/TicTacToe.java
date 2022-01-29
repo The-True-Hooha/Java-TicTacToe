61a1cd01f26950d6e2292b005072b6e665d88d20
David Ogar <owogogahhero@outlook.com>
2022-01-29 23:32:14 +0100

    added new lines to the method and completed the app

--- src/com/company/TicTacToe.java
+++ src/com/company/TicTacToe.java
@@ -116,16 +116,118 @@ public class TicTacToe implements ActionListener {
                         (buttons[3]).getText() == "X"
                         ) {
             xWins(0, 1, 2);
+        } if (
+                (buttons [3].getText() == "X") &&
+                        (buttons [4].getText() == "X") &&
+                        (buttons [5].getText() == "X")
+        ){
+            xWins(3, 4, 5);
+        } if (
+                (buttons [6].getText() == "X") &&
+                        (buttons [7].getText() == "X") &&
+                        (buttons [8].getText() == "X")
+        ){
+            xWins(6, 7, 8);
+        } if (
+                (buttons [0].getText() == "X") &&
+                        (buttons [3].getText() == "X") &&
+                        (buttons [6].getText() == "X")
+        ){
+            xWins(0, 3, 6);
+        } if (
+                (buttons [1].getText() == "X") &&
+                        (buttons [4].getText() == "X") &&
+                        (buttons [7].getText() == "X")
+        ){
+            xWins(1, 4, 7);
+        } if (
+                (buttons [2].getText() == "X") &&
+                        (buttons [5].getText() == "X") &&
+                        (buttons [8].getText() == "X")
+        ){
+            xWins(2, 5, 8);
+        } if (
+                (buttons [0].getText() == "X") &&
+                        (buttons [4].getText() == "X") &&
+                        (buttons [8].getText() == "X")
+        ){
+            xWins(0, 4, 8);
+        } if (
+                (buttons [2].getText() == "X") &&
+                        (buttons [4].getText() == "X") &&
+                        (buttons [6].getText() == "X")
+        ){
+            xWins(2, 4, 6);
+        }
+        if (
+                (buttons[0].getText() == "O") &&
+                        (buttons[1].getText() == "O") &&
+                        (buttons[2]).getText() == "O" &&
+                        (buttons[3]).getText() == "O"
+        ) {
+            oWins(0, 1, 2);
+        } if (
+                (buttons [3].getText() == "O") &&
+                        (buttons [4].getText() == "O") &&
+                        (buttons [5].getText() == "O")
+        ){
+            oWins(3, 4, 5);
+        } if (
+                (buttons [6].getText() == "O") &&
+                        (buttons [7].getText() == "O") &&
+                        (buttons [8].getText() == "O")
+        ){
+            oWins(6, 7, 8);
+        } if (
+                (buttons [0].getText() == "O") &&
+                        (buttons [3].getText() == "O") &&
+                        (buttons [6].getText() == "O")
+        ){
+            oWins(0, 3, 6);
+        } if (
+                (buttons [1].getText() == "O") &&
+                        (buttons [4].getText() == "O") &&
+                        (buttons [7].getText() == "O")
+        ){
+            oWins(1, 4, 7);
+        } if (
+                (buttons [2].getText() == "O") &&
+                        (buttons [5].getText() == "O") &&
+                        (buttons [8].getText() == "O")
+        ){
+            oWins(2, 5, 8);
+        } if (
+                (buttons [0].getText() == "O") &&
+                        (buttons [4].getText() == "O") &&
+                        (buttons [8].getText() == "O")
+        ){
+            oWins(0, 4, 8);
+        } if (
+                (buttons [2].getText() == "O") &&
+                        (buttons [4].getText() == "O") &&
+                        (buttons [6].getText() == "O")
+        ){
+            oWins(2, 4, 6);
         }
 
     }
 
     //method to check if X or O wins
     public void xWins (int a, int b, int c) {
+        buttons[a].setBackground(Color.GREEN);
+        buttons[b].setBackground(Color.GREEN);
+        buttons[c].setBackground(Color.GREEN);
 
+        for (int i = 0; i < 9; i++) {
+            buttons[i].setEnabled(false);
+        }
+        textField.setText("X wins the game");
     }
 
     public void oWins (int a, int b, int c) {
 
+        for (int i = 0; i < 9; i++) {
+        }
+        textField.setText("O wins the game");
     }
 }
